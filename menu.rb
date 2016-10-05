3da85efe071f207275078d7e33eeb80ae218dc63
Rachel van de Pol <rachelvdpol@gmail.com>
2016-10-05 11:51:19 +0200

    Add dishes

---
+++ menu.rb
@@ -0,0 +1,7 @@
+require './dish'
+
+class Menu
+  def initialize(dish_name)
+    @name = dish_name
+
+end
--- waiter.rb
+++ waiter.rb
@@ -1,8 +1,24 @@
 class Waiter
-end
-
-class Waiter
   def greet_guest
-  p "Good day. Welcome to our lovely restaurant."
+    p "Good day. Welcome to our lovely restaurant."
+  end
+
+  def serve_guest
+    p "How can i be of service"
+    p "1. Woud you like to order a pizza?"
+    p "2. Would you like to leave?"
+    choice = gets.chomp.to_i
+    take_order(choice)
+  end
+
+  def take_order(order_number)
+    case order_number
+    when 1
+      p "Let me get the menu"
+    when 2
+      p "Thank you for your visit"
+    else
+      p "I really don't understand"
+    end
   end
 end
